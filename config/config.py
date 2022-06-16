71affa323ac01d5b3ed007a1a895ef3bf1ac14f8
Unknown Mortal <104091630+UnknownMortal@users.noreply.github.com>
2022-06-16 14:48:19 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import re
 import sys
 from os import getenv
@@ -56,7 +47,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TeamYukki/YukkiMusicBot",
+    "https://github.com/UnknownMortal/Yukki-Clone",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
